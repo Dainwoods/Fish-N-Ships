d096f406cbf173dbc7e0bbaae181c3ccad79a50b
dain woods <dain woods>
2017-10-21 14:17:59 -0400

    please work

--- Assets/_Completed-Assets/Scripts/EnemySpawner.cs
+++ Assets/_Completed-Assets/Scripts/EnemySpawner.cs
@@ -6,16 +6,32 @@ public class EnemySpawner : MonoBehaviour {
 
     public GameObject swordfish;
     public int[][] tileArr;
+    public GameObject player;
+    public GameObject point;
+
+    private float timer;
 
 	// Use this for initialization
 	void Start () {
-		
+        Invoke("spawnEnemies", 5);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void spawnEnemies() {
+        point.transform.position = player.transform.position;
+        timer = Time.deltaTime;
+        int playX = (int)player.transform.position.x;
+        int playY = (int)player.transform.position.y;
+        for(int x = -9; x < 9; x++) {
+            for(int y = -7; y < 7; y++) {
+                float rand = Random.Range(0, 4);
+                if(rand > 1) {
+                    GameObject sf = (GameObject)Instantiate(swordfish);
+                    sf.GetComponent<SwordfishAttack>().startPos(playX + x + .5f, playY + y + .5f, this.gameObject);
+                }
+            }
+        }
+    }
 
     public void tileUpdate(int[][] tiles) {
 
--- Assets/_Completed-Assets/Scripts/SwordfishAttack.cs
+++ Assets/_Completed-Assets/Scripts/SwordfishAttack.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class SwordfishAttack : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public Collider2D swordCol;
+    private GameObject player;
+    public Sprite swordfish;
+
+    private Collider2D playCol;
+    private float growScale;
+    private bool grounded;
+    SpriteRenderer sprRen;
+
+    void Start() {
+        grounded = false;
+        transform.localScale = new Vector3(0, 0, 0);
+        growScale = 0;
+        sprRen = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float timer = Time.deltaTime;
+		if(transform.localScale.x < 1) {
+            transform.localScale = new Vector3(1, 1, 1) * growScale * Time.deltaTime;
+        }
+        else if(!grounded){
+            timer = Time.deltaTime;
+            grounded = true;
+            sprRen.sprite = swordfish;
+            if(swordCol.IsTouching(playCol)) {
+                GetComponent<PlayerState>().takeDamage();
+            }
+        }
+        else if(Time.deltaTime > 3) {
+            Destroy(this.gameObject);
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void startPos(float x, float y, GameObject play) {
+        player = play;
+        playCol = player.GetComponent<Collider2D>();
+        Vector2 pos = new Vector2(x, y);
+        transform.position = pos;
+        growScale = .05f;
+    }
 }
